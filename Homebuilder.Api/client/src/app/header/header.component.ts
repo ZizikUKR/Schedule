95c124df591d8e095bc0d44ce0141c0edc19589b
Nikolay Zinenko <zinich@meta.ua>
2020-01-21 12:33:11 +0200

    fix nav menu

--- Homebuilder.Api/client/src/app/header/header.component.ts
+++ Homebuilder.Api/client/src/app/header/header.component.ts
@@ -1,20 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
+  host: {
+    '(document:click)': 'onClick($event)',
+  }
 })
 export class HeaderComponent implements OnInit {
   toggleView: boolean = false;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private _eref: ElementRef) { }
 
   ngOnInit() {
   }
 
   toggleViewMenu() {
     this.toggleView = !this.toggleView;
+  }
+  onClick(event) {
+    if (!this._eref.nativeElement.contains(event.target)) {
+      this.toggleView = false;
+    }
   }
 }
