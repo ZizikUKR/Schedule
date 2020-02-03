03e2b7642ac0e0e8da715d62c169d31e84754987
Nikolay Zinenko <zinich@meta.ua>
2020-02-03 17:50:25 +0200

    remade header

--- Homebuilder.Api/client/scheduler/src/index.js
+++ Homebuilder.Api/client/scheduler/src/index.js
@@ -1,10 +1,3 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.scss';
-// import App from './App';
-
-// ReactDOM.render(<App />, document.getElementById('root'));
-
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
